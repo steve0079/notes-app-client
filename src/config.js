35d278747b4ffe7feeb5aa75d39c69ff08d86ba5
steve0079 <49959235+steve0079@users.noreply.github.com>
2019-10-03 11:43:36 -0700

    Secondary Commit

---
+++ src/config.js
@@ -0,0 +1,19 @@
+export default {
+    
+    MAX_ATTACHMENT_SIZE: 5000000,
+
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "steve-0079-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://ukmtwbwrxc.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_gEqU854WQ",
+        APP_CLIENT_ID: "6usah1irnpsq4kr11co3v0mvek",
+        IDENTITY_POOL_ID: "us-east-1:2cb23e22-a6f6-4203-b2c5-dc27e918cf57"
+    }
+};
---
+++ src/libs/awsLib.js
@@ -0,0 +1,15 @@
+import { Storage } from "aws-amplify";
+
+export async function s3Upload(file) {
+    const filename = `${Date.now()}-${file.name}`;
+
+    const stored = await Storage.vault.put(filename, file, {
+        contentType: file.type
+    });
+
+    return stored.key;
+}
+
+export async function s3Remove(file) {
+    await Storage.vault.remove(file);
+}
