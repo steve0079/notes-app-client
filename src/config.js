ece1e6f6b80284fd2b4688b23230db9b139a9c7e
steve0079 <49959235+steve0079@users.noreply.github.com>
2019-10-03 13:17:05 -0700

    Adding a billing form

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
     
     MAX_ATTACHMENT_SIZE: 5000000,
-
+    STRIPE_KEY: "pk_test_NJvJkCnKUBUO3uLT4UrlZSjC00EypszFOT",
     s3: {
         REGION: "us-east-1",
         BUCKET: "steve-0079-notes-app-uploads"
@@ -15,11 +15,5 @@ export default {
         USER_POOL_ID: "us-east-1_gEqU854WQ",
         APP_CLIENT_ID: "6usah1irnpsq4kr11co3v0mvek",
         IDENTITY_POOL_ID: "us-east-1:2cb23e22-a6f6-4203-b2c5-dc27e918cf57"
-    },
-    dev: {
-        STRIPE_KEY: "pk_test_NJvJkCnKUBUO3uLT4UrlZSjC00EypszFOT",
-    },
-    prod: {
-        STRIPE_KEY: "pk_test_NJvJkCnKUBUO3uLT4UrlZSjC00EypszFOT",
     }
-};
+}
